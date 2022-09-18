51c23ca289a8b064aec431c2e2ef16382158352e
theGreatKapybara <oliver.shustar@gmail.com>
2022-09-18 16:07:34 +0500

    Counter made

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
 ﻿string[] array = {"Hello","23","Hi","Fish"};
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i].Length <3)
+//     {
+//         int count = 0;
+//         count++;
+//         string[] LessThreeSymbols = new string[count];
+        
+//         LessThreeSymbols[count] = array[i];
+        
+//     }
+// }
+static int Counter(string[] array)
+{
+    
+    int count = new int();
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
